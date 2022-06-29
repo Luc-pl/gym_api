e1e2a4fc0b37f14a5db7de6dee9ab2b9ff9f53ac
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-06-29 22:22:53 +0200

    added Hero component

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Box } from '@mui/material';
+
 import './App.css';
 import ExerciseDetail from './pages/ExerciseDetail';
 import Home from './pages/Home';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 
+const theme = createTheme({
+  typography: {
+    fontFamily: [
+      'Anek Latin',
+      'sans-serif'
+    ].join(','),
+  }
+});
+
 const App = () => {
   return (
-    <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
+    <ThemeProvider theme={theme}>
+      <Box width="400px" sx={{ width: { xl: '1488px' }}} m="auto">
         <Navbar />
         <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/exercise/:id" element={<ExerciseDetail />} /> 
+          <Route path="/" element={<Home />} />
+          <Route path="/exercise/:id" element={<ExerciseDetail />} /> 
         </Routes>
         <Footer />
-    </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
 
