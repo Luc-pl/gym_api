362ec8064497934c29e29b7c64d21309ae9aef7b
Łukasz Mijała <lukasz.mijala@gmail.com>
2022-07-11 23:09:26 +0200

    added horizontall scrolball arrows

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  Stack, /*Typography*/ } from '@mui/material';
+import { Stack, Typography } from '@mui/material';
 import Icon from '../assets/icons/gym.png';
 
 const BodyPart = ({ item, setBodyPart, bodyPart }) => {
@@ -16,9 +16,20 @@ const BodyPart = ({ item, setBodyPart, bodyPart }) => {
         width: '270px',
         height: '280px',
         cursor: 'pointer',
-        gap: '47'}}
+        gap: '47'
+      }}
+      onClick={() => {
+        setBodyPart(item);
+        window.scrollTo({top: 1800, left: 100, behavior: 'smooth'})
+      }}
     >
-      <img src={Icon} alt="dumbbell" styles={{ width: '40px', height: '40px'}} />
+      <img src={Icon} alt="dumbbell" style={{ width: '40px', height: '40px'}} />
+      <Typography
+        fontSize='24px'
+        fontWeight='bold'
+        color="#3A1212"
+        textTransform="capitalize"
+      >{item}</Typography>
     </Stack>
   )
 }
